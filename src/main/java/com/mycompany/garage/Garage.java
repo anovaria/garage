4fae5808602683642ffc5f2c60ab358085cb6bf5
alex <alex@alex-Lenovo-G50-45>
2019-12-15 23:55:57 +0100

    vers1

--- src/main/java/com/mycompany/garage/Garage.java
+++ src/main/java/com/mycompany/garage/Garage.java
@@ -1,69 +1,100 @@
 package com.mycompany.garage;
-import java.util.Arrays;
-import org.apache.commons.lang.ArrayUtils;
+
 /**
  *
  * @author alex
  */
 public class Garage {
 
-    private static Auto elencoAuto[];
+    static Auto elencoAuto[];
     private static Furgone elencoFurgoni[];
     private static Moto elencoMoto[];
-    private final int maxPosti = 3;
+    private final int maxPosti = 4;
 
     public static void main(String[] args) {
         Garage G = new Garage();
     }
-    public void rimuoviA(Auto A){
-        elencoAuto=(Auto[]) ArrayUtils.remove(elencoAuto, 0);
-        System.out.println("Rimosso: "+A.toString());
+
+    public void rimuoviA(Auto A) {
+
+        elencoAuto[A.posto] = new Auto(A.posto, 0, "", "", 0, 0);
+        System.out.println("Rimosso: " + A.toString());
+        stampaSituazione();
     }
-    public void rimuoviM(Moto M){
-        elencoMoto=(Moto[]) ArrayUtils.remove(elencoMoto, 0);
-        System.out.println("Rimosso: "+M.toString());
+
+    public void rimuoviM(Moto M) {
+        elencoMoto[M.posto] = new Moto(M.posto, 0, "", 0, 0);
+        System.out.println("Rimosso: " + M.toString());
+        stampaSituazione();
     }
-    public void rimuoviF(Furgone F){
-        
+
+    public void rimuoviF(Furgone F) {
+        elencoFurgoni[F.posto] = new Furgone(F.posto, 0, "", 0, 0);
+        System.out.println("Rimosso: " + F.toString());
+        stampaSituazione();
     }
-    
+
+    public void stampaSituazione() {
+        String res = "Situazione Auto in garage:";
+        for (Auto elencoAuto1 : elencoAuto) {
+            res += "\n" + elencoAuto1.toString();
+        }
+        res += "\nSituazione Moto in garage:";
+        for (Moto elencoMoto1 : elencoMoto) {
+            res += "\n" + elencoMoto1.toString();
+        }
+        res += "\nSituazione Furgoni in garage:";
+        for (Furgone elencoFurgoni1 : elencoFurgoni) {
+            res += "\n" + elencoFurgoni1.toString();
+        }
+        System.out.println(res);
+    }
+
     public void inserisci(Auto A) {
-        int posto=0;
-        if (controlla(posto)) {
-            elencoAuto[posto] = A;
-            
-            System.out.println("Inserito: "+A.toString());
+        int posto = 0;
+        if (controlla(A.posto)) {
+            elencoAuto[A.posto] = A;
+
+            System.out.println("Inserito: " + A.toString());
+            stampaSituazione();
         } else {
 
             System.out.println("posi pieni");
+            stampaSituazione();
         }
     }
 
     public void inserisci(Moto M) {
-        int posto=0;
-        if (controlla(posto)) {
-            elencoMoto[posto] = M;
-            System.out.println("Inserito: "+M.toString());
+        int posto = 0;
+        if (controlla(M.posto)) {
+            elencoMoto[M.posto] = M;
+            System.out.println("Inserito: " + M.toString());
+            stampaSituazione();
         } else {
 
             System.out.println("posi pieni");
+            stampaSituazione();
         }
     }
 
     public void inserisci(Furgone F) {
-        int posto=0;
-        if (controlla(posto)) {
-            elencoFurgoni[posto] = F;
-            System.out.println("Inserito: "+F.toString());
+        int posto = 0;
+        if (controlla(F.posto)) {
+            elencoFurgoni[F.posto] = F;
+            System.out.println("Inserito: " + F.toString());
+            stampaSituazione();
         } else {
             System.out.println("posi pieni");
+            stampaSituazione();
         }
     }
-/**
- * controllo disponibilita posti
+
+    /**
+     * controllo disponibilita posti
+     *
      * @param posto
- * @return 
- */
+     * @return
+     */
     public boolean controlla(int posto) {
         int cont = 0;
 
@@ -95,27 +126,27 @@ public class Garage {
         // preparo gli array
         elencoAuto = new Auto[maxPosti];
         for (int i = 0; i < elencoAuto.length; i++) {
-            elencoAuto[i] = new Auto(i,0, "", "", 0, 0);
+            elencoAuto[i] = new Auto(i, 0, "", "", 0, 0);
         }
         elencoFurgoni = new Furgone[maxPosti];
         for (int i = 0; i < elencoFurgoni.length; i++) {
-            elencoFurgoni[i] = new Furgone(i,0, "", 0, 0);
+            elencoFurgoni[i] = new Furgone(i, 0, "", 0, 0);
         }
         elencoMoto = new Moto[maxPosti];
         for (int i = 0; i < elencoMoto.length; i++) {
-            elencoMoto[i] = new Moto(i,0, "", 0, 0);
+            elencoMoto[i] = new Moto(i, 0, "", 0, 0);
         }
-        
+
         //inserisco veicoli
-        Auto A = new Auto(0, 4, "benz",  "fiat", 2000, 1200);
+        Auto A = new Auto(3, 4, "benz", "fiat", 2000, 1200);
         inserisci(A);
-        Moto M = new Moto(1, 1, "aprilia", 2000, 600);
+        Moto M = new Moto(2, 1, "aprilia", 2000, 600);
         inserisci(M);
-        Furgone F = new Furgone(2,300, "Ducato", 1995,2500);
+        Furgone F = new Furgone(2, 300, "Ducato", 1995, 2500);
         inserisci(F);
-        rimuoviA(elencoAuto[0]);
-        rimuoviM(elencoMoto[0]);
-        rimuoviF(elencoFurgoni[0]);
+        rimuoviA(elencoAuto[3]);
+        rimuoviM(elencoMoto[2]);
+        rimuoviF(elencoFurgoni[2]);
     }
 }
 
@@ -126,8 +157,8 @@ class Veicolo {
     int anno;
     int cilindrata;
 
-    public Veicolo(int posto,String marca, int anno, int cilindrata) {
-        this.posto= posto;
+    public Veicolo(int posto, String marca, int anno, int cilindrata) {
+        this.posto = posto;
         this.marca = marca;
         this.anno = anno;
         this.cilindrata = cilindrata;
@@ -139,15 +170,15 @@ class Auto extends Veicolo {
     public int porte;
     public String alim;
 
-    public Auto(int posto,int porte, String alim, String marca, int anno, int cilindrata) {
-        super(posto,marca, anno, cilindrata);
+    public Auto(int posto, int porte, String alim, String marca, int anno, int cilindrata) {
+        super(posto, marca, anno, cilindrata);
         this.porte = porte;
         this.alim = alim;
     }
 
     @Override
     public String toString() {
-        return "posto: "+posto+") Auto: " + marca + ", porte=" + porte + ", alim=" + alim + ", anno=" + anno + ", "
+        return "posto: " + posto + ") Auto: " + marca + ", porte=" + porte + ", alim=" + alim + ", anno=" + anno + ", "
                 + "cil.=" + cilindrata;
     }
 
@@ -157,14 +188,14 @@ class Moto extends Veicolo {
 
     int tempi;
 
-    public Moto(int posto,int tempi, String marca, int anno, int cilindrata) {
-        super(posto,marca, anno, cilindrata);
+    public Moto(int posto, int tempi, String marca, int anno, int cilindrata) {
+        super(posto, marca, anno, cilindrata);
         this.tempi = tempi;
     }
 
     @Override
     public String toString() {
-        return "posto: "+posto+") Moto " + marca + " cil.=" + cilindrata + " anno=" + anno + " tempi=" + tempi + '}';
+        return "posto: " + posto + ") Moto " + marca + " cil.=" + cilindrata + " anno=" + anno + " tempi=" + tempi + '}';
     }
 
 }
@@ -173,14 +204,14 @@ class Furgone extends Veicolo {
 
     int cpacita;
 
-    public Furgone(int posto,int cpacita, String marca, int anno, int cilindrata) {
-        super(posto,marca, anno, cilindrata);
+    public Furgone(int posto, int cpacita, String marca, int anno, int cilindrata) {
+        super(posto, marca, anno, cilindrata);
         this.cpacita = cpacita;
     }
 
     @Override
     public String toString() {
-        return "posto: "+posto+") Furgone" + " marca=" + marca + " cpacita=" + cpacita + " anno=" + anno
+        return "posto: " + posto + ") Furgone" + " marca=" + marca + " cpacita=" + cpacita + " anno=" + anno
                 + " cilind.=" + cilindrata;
     }
 
